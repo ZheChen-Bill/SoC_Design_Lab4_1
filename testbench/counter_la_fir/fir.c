22750c62ce5e328b682aaa241f56a63a09747a4e
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-10-29 04:18:29 -0400

    First commit

---
+++ testbench/counter_la_fir/fir.c
@@ -0,0 +1,32 @@
+#include "fir.h"
+
+void __attribute__ ( ( section ( ".mprjram" ) ) ) initfir() {
+	// initial your fir
+	for(int i=0; i<N; i=i+1){
+		inputbuffer[i] = 0;
+		outputsignal[i] = 0;
+	}
+}
+int __attribute__ ( ( section ( ".mprjram" ) ) ) output(int input) {
+	// count fir output
+	for(int i=0; i<(N-1); i=i+1){
+		// x[0] => x[1] x[0] => x[2] x[1] x[0] => x[3] x[2] x[1] x[0] ... etc
+		inputbuffer[i+1] = inputbuffer[i];
+	}
+	inputbuffer[0] = input;
+
+	int outputdata = 0;
+	for (int i=0; i<N; i=i+1){
+		outputdata += taps[i] * inputbuffer[i];
+	}
+	return outputdata;
+}
+int* __attribute__ ( ( section ( ".mprjram" ) ) ) fir(){
+	initfir();
+	//write down your fir
+
+	for(int i=0; i<N; i=i+1){
+		outputsignal[i] = output(inputsignal[i]);
+	}
+	return outputsignal;
+}
